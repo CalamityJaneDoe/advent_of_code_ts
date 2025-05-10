e187a953536b3585252d18ed59e6b4ec145ee510
Calamity Jane <calamityjane@Boadicee.local>
2025-05-10 14:58:30 +0200

    day 1 - 2015

--- 2015/src/day-1.ts
+++ 2015/src/day-1.ts
@@ -9,9 +9,43 @@ import * as fs from "fs";
 
 // *************** FIRST EXERCISE ***************
 
+/*
+Iterate through str and add 1 floor if "(" and -1 floor if ")"
+Less straightforward but more readable syntax :
 const findTheFloor = (str :string) :number => {
-    let floorNumber
+    let floor = 0
+    for (const el of str) {
+        if (el === '(')
+            floor ++
+        else
+            floor --
+    }
+    return floor
+}
+*/
+const findTheFloor = (str :string): number =>
+    str.split("")
+        .reduce((floor :number, el :string) => floor + (el === '(' ? 1 : -1), 0)
+
+let rawData :string = fs.readFileSync('2015/inputs/1.txt', 'utf8')
+console.log(findTheFloor(rawData))
+
+// *************** SECOND EXERCISE ***************
+
+const findThePosition = (str :string) :number => {
+    let floor :number = 0
+    let position :number = 0
+    for (const el of str) {
+        if (el === '(' && floor >= 0) {
+            floor++;
+            position++
+        } else if (floor >= 0) {
+            floor--
+            position++
+        }
+    }
+    return position
 }
 
-
-let rawData :string = fs.readFileSync('2024/inputs/1.txt', 'utf8')
+rawData = fs.readFileSync('2015/inputs/1.txt', 'utf8')
+console.log(findThePosition(rawData))
